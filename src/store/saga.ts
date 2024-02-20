894d7eb69eba227786709527c4cdfacfd1c2d9b9
Maykonctba <maykonctba@gmail.com>
2024-02-20 14:47:31 -0300

    eslint-prettierrc-editorconfig

--- src/store/saga.ts
+++ src/store/saga.ts
@@ -3,14 +3,24 @@ import {fetchGithubData} from '../api/github';
 import { fetchDataSuccess } from './actions';
 import { AnyAction } from 'redux-saga';
 
-function* fetchData(action: AnyAction): Generator<any, void, any>{
+interface Data {
+    loginName: string | null
+    blogUrl: string | null
+    login: string | null
+}
+
+function* fetchData(action: AnyAction): Generator<unknown, void, Data>{
     try {
         const login = action.payload;
         const data = yield call(fetchGithubData, login);
         yield put(fetchDataSuccess(data));
         console.log('<------Data fetched ESTA AQUI DE PROPOSITO',data);
-    } catch (error: any) {
-        yield put({type: "FETCH_DATA_ERROR", error});
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            yield put({type: "FETCH_DATA_ERROR", error: error.message});
+        } else {
+            yield put({type: "FETCH_DATA_ERROR", error: "An unknown error occurred"});
+        }
     }
 }
 
